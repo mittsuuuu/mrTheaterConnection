767845e9d45bfcd59157f38128bceb07c62e1d8f
mitsuru <mitsuru.bamitonto@gmail.com>
2024-07-12 15:24:55 +0900

    通信の初期設定をするメソッドの追加

---
+++ Assets/Scripts/userDB.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userDB : MonoBehaviour
+{
+    string IP;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // DBにデータを登録する用のメソッド
+    public void RegisterData()
+    {
+
+    }
+}
