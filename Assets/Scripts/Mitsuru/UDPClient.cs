33629d72ccb088336d60a35acf85920687aced1d
sit21A3079 <21a3079@sit.shonan-it.ac.jp>
2024-08-01 14:56:33 +0900

    UDPClientがid+Transformでデータを送信するようにした

--- Assets/Scripts/Mitsuru/UDPClient.cs
+++ Assets/Scripts/Mitsuru/UDPClient.cs
@@ -1,10 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
 
 public class UDPClient : MonoBehaviour
 {
+    TCPServer tcpServer;
+
+    UdpClient udpClient;
+
+    string ip;
+    int port;
     int id;
 
-    public void setId(int id) { }
+    private void Start()
+    {
+        ip = tcpServer.server_ip;
+        port = tcpServer.server_port;
+
+        udpClient = new UdpClient(ip, port);
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+
+        Task.Run(() => SendData());
+    }
+
+    void SendData()
+    {
+        while(true)
+        {
+            var message = Encoding.UTF8.GetBytes(id.ToString() + "," + transform.ToString());
+            udpClient.Send(message, message.Length);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        udpClient.Dispose();
+    }
 }
